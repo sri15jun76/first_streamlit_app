1d9b9b19416a1b945c98d462d4861574ba57fd04
Srinivas Reddy <115101598+sri15jun76@users.noreply.github.com>
2022-10-05 18:26:50 +0100

    Update streamlit_app.py
    Added few new lines

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('My gym is New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('First message')
+streamlit.text('Second message')
+streamlit.text('Third message')
+
