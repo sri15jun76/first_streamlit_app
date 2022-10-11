fe6d2f73c49c5df23a54453891d37d540c0fb094
Srinivas Reddy <115101598+sri15jun76@users.noreply.github.com>
2022-10-11 21:42:41 +0100

    Update streamlit_app.py
    Changed title

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import snowflake.connector
 
 
 
-streamlit.title('My gym is New Healthy Diner')
+streamlit.title("Reddy's New Healthy Diner")
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
